27b089b79d1140365401de597b7878313069d39e
kaimingchan <chan.kaiming@gmail.com>
2023-08-02 10:23:55 +0800

    fixed routes

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   }, []);
 
   return (
-    <Router basename="My-Portfolio">
+    <Router basename="My-new-Portfolio">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
